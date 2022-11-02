ba2c2527c0aa0f4505105a7bf22d2dbeb3774d1f
Andriy Skachko <96697847+andriyskachko@users.noreply.github.com>
2022-11-02 17:38:16 +0200

    fixed region info to use lightning-form

--- force-app/main/default/lwc/regionInfo/regionInfo.js
+++ force-app/main/default/lwc/regionInfo/regionInfo.js
@@ -3,7 +3,19 @@ import getRegionById from '@salesforce/apex/RegionController.getRegionById';
 import ChangeRegionalManagerModal from 'c/changeRegionalManagerModal';
 import './regionInfo.css';
 
+import REGION_OBJECT from '@salesforce/schema/Region__c';
+import REGION_NAME_FIELD from '@salesforce/schema/Region__c.Name';
+import REGIONAL_MANAGER_FIELD from '@salesforce/schema/Region__c.Regional_ManagerId__c';
+
+const FIELDS = [
+  REGION_NAME_FIELD.fieldApiName,
+  REGIONAL_MANAGER_FIELD.fieldApiName
+];
+
 export default class RegionInfo extends LightningElement {
+  fields = FIELDS;
+  objectApiName = REGION_OBJECT.objectApiName;
+
   /** @type {string} */
   regionId;
 
