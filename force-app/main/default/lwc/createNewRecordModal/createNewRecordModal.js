e640a764a7e16b8e3e38858710fb176e844c65b7
Andriy Skachko <96697847+andriyskachko@users.noreply.github.com>
2022-11-07 20:40:17 +0200

    cleaned up createNewRecordModal component

--- force-app/main/default/lwc/createNewRecordModal/createNewRecordModal.js
+++ force-app/main/default/lwc/createNewRecordModal/createNewRecordModal.js
@@ -8,24 +8,25 @@ export default class CreateNewRecordModal extends LightningModal {
   @api objectApiName = '';
   @api fields = [];
 
-  get heading() {
-    return `Add new ${this.objectName}`;
-  }
-
-  handleCancel() {
-    this.close();
-  }
-
   handleSubmit(event) {
     event.preventDefault();
     const fields = event.detail.fields;
     this.props.forEach((prop) => {
       fields[prop.fieldName] = prop.value;
     });
+
     this.template.querySelector('lightning-record-form').submit(fields);
   }
 
   handleSuccess(event) {
     this.close(event.detail.id);
+  }
+
+  handleCancel() {
+    this.close();
+  }
+
+  get label() {
+    return `Add new ${this.objectName}`;
   }
 }
