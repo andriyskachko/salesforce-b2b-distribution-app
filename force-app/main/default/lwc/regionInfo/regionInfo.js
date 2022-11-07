9ef180eaadf46c7ff14f84a7648514a25516681a
Andriy Skachko <96697847+andriyskachko@users.noreply.github.com>
2022-11-07 15:21:18 +0200

    unused properties

--- force-app/main/default/lwc/regionInfo/regionInfo.js
+++ force-app/main/default/lwc/regionInfo/regionInfo.js
@@ -2,11 +2,11 @@ import { LightningElement } from 'lwc';
 import REGION_OBJECT from '@salesforce/schema/Region__c';
 import REGIONAL_MANAGER_FIELD from '@salesforce/schema/Region__c.Regional_ManagerId__c';
 
-const FIELDS = [REGIONAL_MANAGER_FIELD.fieldApiName];
+const FIELDS = [REGIONAL_MANAGER_FIELD];
 
 export default class RegionInfo extends LightningElement {
   fields = FIELDS;
-  objectApiName = REGION_OBJECT.objectApiName;
+  objectApiName = REGION_OBJECT;
 
   /** @type {string} */
   regionId;
