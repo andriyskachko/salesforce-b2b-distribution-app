465504359ebb83e896edd5801f4584f75fb61c54
Andriy Skachko <96697847+andriyskachko@users.noreply.github.com>
2022-12-02 14:34:53 +0200

    wired sales managers and opportunity results

--- force-app/main/default/lwc/listOfCustomers/listOfCustomers.js
+++ force-app/main/default/lwc/listOfCustomers/listOfCustomers.js
@@ -1,13 +1,14 @@
 import { LightningElement, wire } from 'lwc';
 import getOpportunitiesSummary from '@salesforce/apex/OpportunityController.getOpportunitiesSummary';
+import getSalesManagersForOpportunityAccounts from '@salesforce/apex/OpportunityController.getSalesManagersForOpportunityAccounts';
 
-/** @type {FilterOption} */
+/** @type {Option} */
 const ALL = { value: 'ALL', label: 'All' };
-/** @type {FilterOption} */
+/** @type {Option} */
 const LAST_WEEK = { value: 'LAST_WEEK', label: 'Last week' };
-/** @type {FilterOption} */
+/** @type {Option} */
 const LAST_MONTH = { value: 'LAST_MONTH', label: 'Last month' };
-/** @type {FilterOption} */
+/** @type {Option} */
 const LAST_YEAR = { value: 'LAST_YEAR', label: 'Last year' };
 
 /** @type {DatatableColumn[]} */
@@ -32,31 +33,66 @@ const COLUMNS = [
   }
 ];
 
-/** @type {FilterOption[]} */
+/** @type {Option[]} */
 const FILTER_OPTIONS = [ALL, LAST_WEEK, LAST_MONTH, LAST_YEAR];
 
 export default class ListOfCustomers extends LightningElement {
   /** @type {OpportunitySummaryResultDTO[]} */
   summaryData = [];
-  error;
+  /** @type {AccountSalesManagerDTO[]} */
+  salesManagers = [];
+  wiredSummaryError;
+  wiredSalesManagersError;
   columns = COLUMNS;
   filterOptions = FILTER_OPTIONS;
   currentFilterValue = ALL.value;
   regionId = 'a013N0000051XDVQA2';
+  salesManagerId = '';
 
   @wire(getOpportunitiesSummary, {
     regionId: '$regionId',
-    closeDateFilter: '$currentFilterValue'
+    closeDateFilter: '$currentFilterValue',
+    salesManagerId: '$salesManagerId'
   })
   wiredSummaryData({ error, data }) {
     if (data) {
       this.summaryData = data;
-      this.error = undefined;
+      this.wiredSummaryError = undefined;
       console.log(this.summaryData);
     } else if (error) {
       this.summaryData = [];
-      this.error = error;
-      console.log(this.error);
+      this.wiredSummaryError = error;
+      console.log(this.wiredSummaryError);
     }
+  }
+
+  @wire(getSalesManagersForOpportunityAccounts, {
+    regionId: '$regionId',
+    closeDateFilter: '$currentFilterValue',
+    salesManagerId: '$salesManagerId'
+  })
+  wiredSalesManagers({ error, data }) {
+    if (data) {
+      this.salesManagers = data;
+      this.wiredSalesManagersError = undefined;
+      console.log(this.salesManagers);
+    } else if (error) {
+      this.salesManagers = [];
+      this.wiredSalesManagersError = error;
+      console.log(this.wiredSalesManagersError);
+    }
+  }
+
+  /** @type {Option[]} */
+  get salesManagerOptions() {
+    return this.salesManagers.map((s) => ({
+      value: s.id,
+      label: s.name
+    }));
+  }
+
+  handleChange(event) {
+    const salesManagerId = event.detail.value;
+    this.salesManagerId = salesManagerId;
   }
 }
